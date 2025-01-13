bd984f19cc9b82b11eec43cb7b4ac0801ee05b73
Babah Kingsley <babahcloud9@gmail.com>
2025-01-13 23:33:11 +0100

    terraform revison first project

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+avz = "eu-central-1c"
+instance_type = "t3.micro"
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+    default = "ami-0233f53a7dafae253" 
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "avz" {
+  
+}
